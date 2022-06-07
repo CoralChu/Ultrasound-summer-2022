bf89ae25ac13738a96c31ccd1ecb9e2e15c98559
Xinying Chu <67030677+CoralChu@users.noreply.github.com>
2022-06-07 09:41:25 -0400

    Add files via upload

---
+++ TVD_2_MAT.m
@@ -0,0 +1,44 @@
+% TVD_2_Mat.m - Script to convert TVD files saved with Telemed EchoWave II
+% software to MAT files
+%
+% Inputs  - You will be prompted to select the TVD files
+% Output  - .mat files with the same name & location as the TVD files
+% containing a structure 'TVDdata' with the fields:
+%         -TVDdata.Im - 3D array representing a stack of 2D grayscale images
+%         -TVDdata.Time - Array of time stamps corresponding to each image
+%         frame
+%         -TVDdata.Height - height of image in pixels
+%         -TVDdata.Width - width of image in pixels
+%         -TVDdata.Fnum - number of image frames
+%
+% Created by Dominic Farris, School of Human Movement Studies, The University of Queensland
+% 18/09/2015. 
+
+clear
+
+% Select the TVD files
+[TVDfiles,TVDpaths] = uigetfile('*.tvd','Select the TVD files to convert','MultiSelect','on');
+
+h = waitbar(0,['Overall Progress... (Processing File 1 of ' num2str(length(TVDfiles)) ')']);
+hp = get(h,'Position');
+set(h,'Position',[hp(1),hp(2)+hp(4),hp(3),hp(4)]);
+
+
+% Read in each files data and write to MAT file
+for i = 1:length(TVDfiles)
+    
+    waitbar(double(i-1)/double(length(TVDfiles)),h,...
+        ['Overall Progress... (Processing File' num2str(i) 'of' num2str(length(TVDfiles)) ')']);
+    
+    TVDdata = readTVDv2([TVDpaths TVDfiles{i}]);
+%     TVDdata = readTVDv2([TVDpaths TVDfiles{i}], [77,44,508,512]);
+    
+    save(strrep([TVDpaths TVDfiles{i}],'.tvd','.mat'),'TVDdata')
+
+    
+    clear TVDdata
+
+
+end
+
+waitbar(1,h,'Conversions Completed')
---
+++ readTVDv2.m
@@ -0,0 +1,103 @@
+function [TVDdata] = readTVDv2(TVDfname, rect)
+% Function to read in a .tvd file created by EchoWave II software and
+% output a stack of grayscale images and the associated time stamps
+%
+% Inputs - TVDfname - string containing full path to .tvd file
+%          
+% Outputs -TVDdata.Im - 3D array representing a stack of 2D grayscale images
+%         -TVDdata.Time - Array of time stamps corresponding to each image
+%         frame
+%         -TVDdata.Height - height of image in pixels
+%         -TVDdata.Width - width of image in pixels
+%         -TVDdata.Fnum - number of image frames
+%
+% Created by Dominic Farris, School of Human Movement Studies, The University of Queensland
+% 13/03/2014. Edited by Glen Lichtwark 06/08/14
+
+% set path to Echo Wave II automation interface client .Net assembly (dll)
+% - I think the function 'computer' returns the version of matlab (32 or 64
+% bit)  and so the commented code doesn't do what you want if you're running 32-bit
+% matlab on a 64-bit platform - as I'm testing on a 64-bit platform (with 32-bit matlab), I'll just
+% specify the path explicitly.
+if strcmp(computer('arch'), 'win64')
+    asm_path = 'C:\Program Files (x86)\TELEMED\Echo Wave II\Config\Plugins\AutoInt1Client.dll';
+else
+    asm_path = 'C:\Program Files\TELEMED\Echo Wave II\Config\Plugins\autoint1client.dll';
+end
+
+% load assembly
+asm = NET.addAssembly(asm_path);
+
+% create commands interface object
+cmd = AutoInt1Client.CmdInt1();
+
+% connect to started Echo Wave II software -UNABLE TO CONNECT!!
+ret = cmd.ConnectToRunningProgram();
+if (ret ~= 0)
+    msgbox('Error. Cannot connect to Echo Wave II software. Please make sure that its options were correctly adjusted and that software is running.', 'Error')
+    return;
+end
+
+%open TVD file (previously saved using Echo Wave II software)
+cmd.OpenFile(TVDfname);
+
+%Get the number of frames in the TVD file
+TVDdata.Fnum = cmd.GetFramesCount();
+%Get the image width
+TVDdata.Width = cmd.GetLoadedFrameWidth();
+%Get the image height
+TVDdata.Height = cmd.GetLoadedFrameHeight();
+
+% this just stops the computer running out of memory - would be good to be
+% able to specifically determine the max value as a function (TO DO)
+if (TVDdata.Fnum > 3000)
+    TVDdata.Fnum = 3000;
+end
+
+% work out how to autocrop the image if no cropping is specified in input
+if nargin < 2
+    cmd.GoToFrame1n(1, true);
+    
+    Im = uint8(cmd.GetLoadedFrameGray());
+    
+    % work out how to trim the edges of the ultrasound (edges are intensity value of
+    % 56]
+    m = find(Im(:,1) == 56); % find where the image starts vertically - at top of image the gray changes from 66 to 56.
+    % n = find(mean((Im(end-50:end,:) == 56))<0.2); % use the averaage value of the bottom 50 rows to determine whether this is a high value (i.e. gray) or low (i.e. real image)
+    n = find(Im(round(size(Im,1)/4*3),:) == 56);
+    
+    %rect = [n(find(diff(n)>1)+1) m(1) n(end)-n(find(diff(n)>1)+1) length(m)-1]; % define the new rectangle for autocroping
+    rect = [min(n(diff(n)>1)) m(1) max(n(find(diff(n)>1)+1))-min(n(diff(n)>1))-1 length(m)-1]; % define the new rectangle for autocroping
+    
+    TVDdata.rect = rect; 
+    TVDdata.Width = rect(3)+1; % save the new width and height
+    TVDdata.Height = rect(4)+1;
+    
+else  % if the cropping box is specified, use it here.
+    TVDdata.rect = rect;
+    TVDdata.Width = rect(3)+1;
+    TVDdata.Height = rect(4)+1;
+end
+
+% go through all frames and save as a UINT8 format
+h = waitbar(0,'Loading ultrasound data ...');
+
+TVDdata.Im = zeros(TVDdata.Height, TVDdata.Width, TVDdata.Fnum,'uint8');
+
+%Loop through the file, storing each frame as a grayscale image
+for i = 1:TVDdata.Fnum;
+    
+    % go to frame and load it
+    cmd.GoToFrame1n(i, true);
+    
+    % Get the time stamp of this frame
+    TVDdata.Time(i) = cmd.GetCurrentFrameTime();
+    
+    % get two-dimensional grayscale array of loaded frame 
+    TVDdata.Im(:,:,i) = imcrop(uint8(cmd.GetLoadedFrameGray()),rect);
+    
+    waitbar(double(i)/double(TVDdata.Fnum),h)
+    
+end
+
+close(h);
